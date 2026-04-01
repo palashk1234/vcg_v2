3307ce8abb7d5564c15ea5b6fa1869eb64318538
Palash K <palash.k1234@gmail.com>
2026-04-01 10:09:10 +0530

    Update webui.py

--- webui.py
+++ webui.py
@@ -559,7 +559,7 @@ if __name__ == "__main__":
     ui.queue().launch(
         server_name="0.0.0.0", 
         server_port=7860, 
-        share=False,
+        share=True,
         inline=False,
         allowed_paths=[OUTPUT_DIR, TEMP_DIR, WORK_DIR, STOCK_DIR]
     )
